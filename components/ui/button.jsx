842f200011fe815b6960cd67b80fb135bd6eda5d
Leo <jr831229@gmail.com>
2025-08-19 19:48:05 +0800

    顏色與按鈕微調

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -12,13 +12,13 @@ const buttonVariants = cva(
         default:
           'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 cursor-pointer',
         highlight:
-          'bg-highlight text-highlight-foreground shadow-xs hover:bg-highlight/90 cursor-pointer',
+          'bg-highlight text-highlight-foreground shadow-xs hover:bg-orange-400 cursor-pointer',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer',
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-xs hover:bg-primary/10 cursor-pointer',
+          'bg-secondary text-secondary-foreground shadow-xs hover:bg-primary/10 dark:hover:bg-primary/20 cursor-pointer',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 cursor-pointer',
         link: 'text-primary underline-offset-4 hover:underline cursor-pointer',
